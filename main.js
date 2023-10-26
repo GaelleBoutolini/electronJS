e5957a819ae97090969788d1f8122f20528d86cd
Hanaline <gaelle.boutolini.simplon@gmail.com>
2023-10-26 21:25:27 +0200

    mise en place d'un nouveau Repo electronJS

---
+++ main.js
@@ -0,0 +1,42 @@
+
+const { app, BrowserWindow, ipcRenderer } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    minWidth: 1400,
+    minHeight: 900,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: true, 
+      contextIsolation: false, 
+      // webPreferences: {
+      //   preload: path.join(__dirname, "preload.js"),
+      // },
+    },
+    icon:'img/app-icon.png'
+  });
+
+  win.loadFile('index.html')
+
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+
+  //pour exécuter mon projet electron, j'ai tapé deux commande:
+  //-npm i electron-packager
+  //npx electron-packager .
+  //npm run package-win
+});
